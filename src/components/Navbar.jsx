b981900ef291e851dcbf073877c80def02c55ac3
Jordan.c.roberts01 <jordan.c.roberts01@gmail.com>
2022-09-04 17:34:50 -0500

    Setup basic files
    -Removed some boilerplate code and stuff

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import { Bars3Icon } from "@heroicons/react/24/outline";
+
+const Navbar = () => {
+  return (
+    <div className="w-screen h-[80px] bg-zinc-800 fixed drop-shadow-lg">
+      <h1 className="text-white">Resume</h1>
+    </div>
+  );
+};
+
+export default Navbar;

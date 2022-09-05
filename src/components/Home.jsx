0f45764ba8df882c62234e7bcd3015cbae74ae91
Jordan.c.roberts01 <jordan.c.roberts01@gmail.com>
2022-09-04 23:40:08 -0500

    Added Background Image for Homepage

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,7 +1,14 @@
 import React from "react";
 
+// import bgImage from "../assets/HomepageBackground.jpg";
+
 const Home = () => {
-  return <div name="Home" className="bg-zinc-600 h-screen w-screen"></div>;
+  return (
+    <div
+      name="Home"
+      className="w-full h-screen bg-[url('../public/images/HomepageBackground.jpg')] bg-no-repeat bg-cover bg-center bg-fixed"
+    ></div>
+  );
 };
 
 export default Home;

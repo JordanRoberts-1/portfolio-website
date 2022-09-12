221e0b22cb6d8720d4c5e145c9e10f84ab693b21
Jordan.c.roberts01 <jordan.c.roberts01@gmail.com>
2022-09-12 03:10:46 -0500

    Added popup when clicking card

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -1,26 +1,75 @@
 import React from "react";
 import LanguageBubble from "./LanguageBubble";
+import { useState } from "react";
+import { XMarkIcon } from "@heroicons/react/24/outline";
 
-const ProjectCard = ({ image, title, languages }) => {
+const ProjectCard = ({
+  preview,
+  image,
+  title,
+  languages,
+  frameworks,
+  description,
+}) => {
+  const [cardExpanded, setCardExpanded] = useState(false);
+  const handleClick = () => {
+    setCardExpanded(!cardExpanded);
+  };
+
   return (
-    <div
-      className="w-[85vw] xs:w-64 border-2 border-zinc-900 rounded-xl h-fit shadow-lg 
+    <>
+      <div
+        className="w-[85vw] xs:w-64 border-2 border-zinc-900 rounded-xl min-h-fit shadow-lg 
      hover:from-black hover:via-neutral-900 hover:to-light shadow-black cursor-pointer bg-gradient-to-b from-black via-neutral-800 to-light flex flex-col
-     transform transition duration-300 hover:scale-110"
-    >
-      <img
-        src={image}
-        className="object-cover h-64 xs:h-[50%] w-full rounded-xl pb-1"
-      />
-      <h1 className="text-white text-xl p-2 border-t-2 border-zinc-800 font-retro text-center">
-        {title}
-      </h1>
-      <div className="flex flex-row flex-1 items-center">
-        {languages.map((language, i) => (
-          <LanguageBubble title={language} key={i} />
-        ))}
+     transform transition duration-300 hover:scale-125"
+        onClick={handleClick}
+      >
+        <img
+          src={preview}
+          className="object-cover h-64 xs:h-[60%] w-full rounded-xl pb-1"
+        />
+        <h1 className="text-white text-xl p-2 border-t-2 border-zinc-800 font-retro text-center">
+          {title}
+        </h1>
+        <div className="flex flex-row flex-1 items-end">
+          {languages.map((language, i) => (
+            <LanguageBubble title={language} key={i} />
+          ))}
+        </div>
       </div>
-    </div>
+      {cardExpanded ? (
+        <div
+          className="fixed z-20 w-[100%] h-screen left-0 top-0 bg-black bg-opacity-50 flex items-center justify-center"
+          onClick={handleClick}
+        >
+          <div
+            className="fixed z-30 w-[80%] h-[90%] bg-gradient-to-b from-neutral-900 via-neutral-800 to-zinc-800 rounded-xl shadow-2xl shadow-black lg:grid lg:grid-cols-2 lg:grid-rows-1 lg:gap-8 overflow-auto"
+            onClick={(e) => e.stopPropagation()}
+          >
+            <div className="h-[100%] p-2 border-zinc-600 hidden lg:inline-block">
+              <img
+                src={image}
+                alt="Flappy Bird Recreated"
+                className="object-cover w-full h-full rounded-xl"
+              />
+            </div>
+            <div className="h-[100%] flex flex-col items-center p-4 gap-8">
+              <h1 className="text-4xl text-center text-zinc-200 mt-8 border-b-2 pb-4 border-b-zinc-200 w-fit font-retro font-semibold">
+                {title}
+              </h1>
+              <p className="text-2xl text-zinc-200 self-start font-sans">
+                {description}
+              </p>
+              <h1 className="text-2xl text-zinc-200 self-start font-sans">{`Frameworks/Libraries: ${frameworks}`}</h1>
+            </div>
+            <XMarkIcon
+              className="absolute z-40 stroke-zinc-200 w-12 top-4 right-4 cursor-pointer transform transition duration-300 hover:rotate-180"
+              onClick={handleClick}
+            />
+          </div>
+        </div>
+      ) : null}
+    </>
   );
 };
 
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,32 +1,44 @@
 import React from "react";
 import placeholder from "../assets/placeholder-flappybird.png";
+import PortfolioPreview from "../assets/portfolio-website-picture.png";
+import PortfolioFull from "../assets/portfolio-website-full.png";
 import ProjectCard from "./ProjectCard";
 
 const Projects = () => {
+  const flappyBirdDescription =
+    "Flappy bird written in low-level C++ and OpenGL which can be played by either a player or an AI. Q-Learning was implemented using a custom Neural Network library to learn how to play the game. The agent can save their training to be loaded and played at a later time.";
+
   return (
     <div
       name="Projects"
-      className="w-full h-fit lg:h-screen bg-gradient-to-b from-black via-zinc-800 to-stone-600 flex flex-col items-center"
+      className="w-full h-fit bg-gradient-to-b from-black via-zinc-800 to-stone-600 flex flex-col items-center min-h-screen"
     >
-      <h1 className="text-white text-5xl mt-32 font-retro mb-2">PROJECTS</h1>
-      <div className="grid grid-cols-1 lg:grid-cols-3 gap-6">
+      <h1 className="text-white text-5xl mt-32 font-retro mb-2 font-bold">
+        PROJECTS
+      </h1>
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 2xl:grid-cols-4 gap-8">
         <ProjectCard
+          preview={placeholder}
           image={placeholder}
           title="AI Plays Homemade Flappy Bird"
           languages={["C++", "GLSL"]}
+          frameworks="OpenGL, glm, imGui, Eigen"
+          description={flappyBirdDescription}
         ></ProjectCard>
         <ProjectCard
-          image={placeholder}
+          preview={PortfolioPreview}
+          image={PortfolioFull}
+          title="Portfolio Website"
+          languages={["JS", "React", "Tailwind"]}
+          frameworks="React, TailwindCSS, react-scroll, typewriter-effect"
+        ></ProjectCard>{" "}
+        <ProjectCard
+          preview={placeholder}
           title="AI Plays Homemade Flappy Bird"
           languages={["C++", "GLSL"]}
         ></ProjectCard>{" "}
         <ProjectCard
-          image={placeholder}
-          title="AI Plays Homemade Flappy Bird"
-          languages={["C++", "GLSL"]}
-        ></ProjectCard>{" "}
-        <ProjectCard
-          image={placeholder}
+          preview={placeholder}
           title="AI Plays Homemade Flappy Bird"
           languages={["C++", "GLSL"]}
         ></ProjectCard>
